3efae02af497b1c04b17425de736362bd8ccaad9
larsk2009 <larsk2009@live.nl>
2019-01-31 00:20:15 +0100

    Initial commit

---
+++ Assets/scripts/RadarManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadarManager : MonoBehaviour
+{
+    public const float RadarUpdateTime = 1f;   // in Seconds
+    private float TimeSinceStart = 0;
+
+    private static RadarManager instance = null;
+
+    private const int standardRange = 40; //Standard range of the radar screen in miles
+    private LineRenderer lineRenderer;
+
+    public RadarRing Circle;
+    public static float WorldPointPerMile
+    {
+        get;
+        private set;
+    }
+
+    void Start()
+    {
+        instance = this;
+        lineRenderer = GetComponent<LineRenderer>();
+
+        int width = Screen.width;
+        int height = Screen.height;
+        Debug.Log("Screen Width: " + width);
+        Debug.Log("Screen Height: " + height);
+
+        //Determine how much miles one pixel needs to be to fit 40 miles (by default) on the screen.
+        WorldPointPerMile = Camera.main.ScreenToWorldPoint(new Vector2(width, height)).x / (float)standardRange;
+
+        DrawRadarUi();
+    }
+
+    private void DrawRadarUi()
+    {
+        //Draw radar rings
+        var clone = Instantiate(Circle);
+        clone.DrawCircle(10 * WorldPointPerMile);
+        clone = Instantiate(Circle);
+        clone.DrawCircle(20 * WorldPointPerMile);
+        clone = Instantiate(Circle);
+        clone.DrawCircle(30 * WorldPointPerMile);
+        clone = Instantiate(Circle);
+        clone.DrawCircle(40 * WorldPointPerMile);
+    }
+
+    void Awake()
+    {
+        if (instance == null)
+        {     // initialize only  one instance.
+            instance = this;
+            GameObject.DontDestroyOnLoad(this.gameObject);
+        }
+        else
+        {                // Destroy unused instances.
+            GameObject.Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeSinceStart = TimeSinceStart + Time.deltaTime;
+        if (TimeSinceStart > RadarUpdateTime)
+        {
+            TimeSinceStart = 0;
+            UpdateRadarScreen();
+        }
+    }
+
+    void UpdateRadarScreen()
+    {
+        var planes = GameObject.FindObjectsOfType(typeof(Airplane));
+
+        foreach (var o in planes)
+        {
+            var plane = (Airplane)o;
+            plane.UpdateLocation();
+        }
+    }
+}
---
+++ Assets/scripts/RadarRing.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class RadarRing : MonoBehaviour
+{
+    private int vertexCount = 10000;
+    private float lineWidth = 0.01f;
+    private float radius;
+
+    private LineRenderer lineRenderer;
+
+    void Awake()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = Color.white;
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.widthMultiplier = lineWidth;
+    }
+
+    public void DrawCircle(float diameter)
+    {
+        radius = diameter / 2;
+        float deltaTheta = (2f * Mathf.PI) / vertexCount;
+        float theta = 0f;
+
+        lineRenderer.positionCount = vertexCount;
+        for (int i = 0; i < lineRenderer.positionCount; i++)
+        {
+            Vector2 pos = new Vector2(radius * Mathf.Cos(theta), radius * Mathf.Sin(theta));
+            lineRenderer.SetPosition(i, pos);
+            theta += deltaTheta;
+        }
+    }
+}
